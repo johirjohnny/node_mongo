1b8b99917689d5c2fc1e80fbe8a831f701b9669c
johirjohnny <johirjohnny96@gmail.com>
2021-03-29 22:19:21 +0600

    node post and get from frontend

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
-
-
+const cors = require('cors')
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/',(req, res) =>{
-    res.send("thank you for calling me , I ko ");
+const users = ["asad", 'galib', 'johnny', 'rony', 'suhana'];
+app.get('/', (req, res) => {
+    res.send("thank you for calling me , hise thamen ");
 })
 
-app.listen(3000, () => console.log('listening to port 3000'));
+
+app.get('/fruits/banana', (req, res) => {
+    res.send({
+        fruits: 'banana', quantity: 1000, price: 1000
+    });
+
+})
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({ id, name });
+})
+
+//post
+app.post('/addUser', (req, res) => {
+    res.send(req.body);
+})
+app.listen(4000, () => console.log('listening to port 4000'));
